dc5f02ba325601909b586e16e3d1b596e46d6e2e
saniok333 <alex.o.csc@gmail.com>
2022-08-30 16:17:21 +0300

    Outputting a List Of Places

--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,8 +1,23 @@
+import { useIsFocused } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import PlacesList from '../components/Places/PlacesList';
 
-const AllPlaces = () => {
-  return <PlacesList />;
+const AllPlaces = ({ route }) => {
+  const [loadedPlaces, setLoadedPlaces] = useState([]);
+
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    if (isFocused && route.params) {
+      setLoadedPlaces((currLoadedPlaces) => [
+        ...currLoadedPlaces,
+        route.params.place,
+      ]);
+    }
+  }, [route, isFocused]);
+
+  return <PlacesList places={loadedPlaces} />;
 };
 
 export default AllPlaces;
